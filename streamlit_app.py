9cc8f85dbba65935e9b74913a5cc570c5c640576
Sukma-dharmanta <sukmadharmantap@gmail.com>
2025-04-24 13:19:17 +0700

    Some history stuff
    Lemao

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("Sukma's History bench")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "Welcome to the website where you can learn many history that doesn't exist in Indonesian textbooks and you can also learn various ways to commit war crimes. Now go to this link where it will lead you to a 1 way ticket to Siberia for free at[docs.streamlit.io](https://docs.streamlit.io/)."
 )
