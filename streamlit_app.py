dab9a05d699d4a0d0afb699bc9a7ea4823486c6d
Sukma-dharmanta <sukmadharmantap@gmail.com>
2025-04-24 13:41:30 +0700

    Picture update

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,5 @@ st.title("Sukma's History bench")
 st.write(
     "Welcome to the website where you can learn many history that doesn't exist in Indonesian textbooks and you can also learn various ways to commit war crimes. Now go to this link where it will lead you to a 1 way ticket to Siberia for free at[docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+st.image("views/images (1) (10).jpeg") 
